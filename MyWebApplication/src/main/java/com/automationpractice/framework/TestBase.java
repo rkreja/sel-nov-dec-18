cfe15853abf20aefbd2dc573480f20f4d302856d
rkreja <rkrejanyc@yahoo.com>
2019-04-09 15:10:26 -0400

    path changed for linux

--- MyWebApplication/src/main/java/com/automationpractice/framework/TestBase.java
+++ MyWebApplication/src/main/java/com/automationpractice/framework/TestBase.java
@@ -8,25 +8,25 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.testng.annotations.AfterMethod;
-import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.Optional;
 import org.testng.annotations.Parameters;
 
 public class TestBase {
 	
-	public WebDriver driver;
+	public static WebDriver driver;
+	
+
 	
 	@BeforeMethod
 	@Parameters({"browser"})
 	public void init(@Optional("chrome") String browser) {
 		
 		if (browser.equals("chrome")) {
-			System.setProperty("webdriver.chrome.driver", "lib\\chromedriver-windows-32bit.exe");
+			System.setProperty("webdriver.chrome.driver", "lib/chromedriver-windows-32bit.exe");
 			driver = new ChromeDriver();
 		}else if(browser.equals("internetexplorer")) {
-			System.setProperty("webdriver.ie.driver", "lib\\internetexplorerdriver-windows-32bit.exe");
+			System.setProperty("webdriver.ie.driver", "lib/internetexplorerdriver-windows-32bit.exe");
 			driver = new InternetExplorerDriver();
 		}else {
 			System.err.println("invalid browser name was given in xml parameter. Valid values are: chrome,internetexplorer");
