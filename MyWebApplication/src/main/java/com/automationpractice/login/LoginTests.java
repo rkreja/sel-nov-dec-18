63bb556d58d56fe04d052285b3ca7f3d5ec3a0a2
rkreja <rkrejanyc@yahoo.com>
2019-04-09 22:13:19 -0400

    commit1

--- MyWebApplication/src/main/java/com/automationpractice/login/LoginTests.java
+++ MyWebApplication/src/main/java/com/automationpractice/login/LoginTests.java
@@ -5,33 +5,43 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import com.automationpractice.framework.TestBase;
+import com.automationpractice.framework.UI;
 
 public class LoginTests extends TestBase{
 	
 	@Test
 	public void login_with_vaid_userid_and_password() {
 			
+		
 		//Go to http://www.automationpractice.com
 		driver.get("http://www.automationpractice.com");
 		
 		//Click Sign in button from top right corner of the page
-		driver.findElement(By.className("login")).click();
+		//driver.findElement(By.className("login")).click();
+		UI.clickByClass("login");
 		
 		//Enter valid email address 'abc213@mailinator.com' in email address text field of right side
-		driver.findElement(By.id("email")).sendKeys("abc213@mailinator.com");
+//		driver.findElement(By.id("email")).sendKeys("abc213@mailinator.com");
+		UI.typeById("email", "abc213@mailinator.com");
 		
 		//Enter valid password 'abc1234' in password text field
-		driver.findElement(By.id("passwd")).sendKeys("abc1234");
+//		driver.findElement(By.id("passwd")).sendKeys("abc1234");
+		UI.typeById("passwd", "abc1234");
 		
+		
+		UI.typeById("seccode", "xyz");
 		//Click 'Sign in' button
-		driver.findElement(By.id("SubmitLogin")).click();
+//		driver.findElement(By.id("SubmitLogin")).click();
+		UI.clickById("SubmitLogin");
 		
 		//Verify 'MY ACCOUNT' text displayed
-		String myaccounttxt = driver.findElement(By.className("page-heading")).getText();
+//		String myaccounttxt = driver.findElement(By.className("page-heading")).getText();
+		String myaccounttxt = UI.getTextById("page-heading");
 		Assert.assertEquals(myaccounttxt, "MY ACCOUNT");
 		
 		//Verify 'Welcome to your account. Here you can manage all of your personal information and orders.' text displayed
-		String wwelcomeMsg=driver.findElement(By.className("info-account")).getText();
+//		String wwelcomeMsg=driver.findElement(By.className("info-account")).getText();
+		String wwelcomeMsg=UI.getTextByClass("info-account");
 		Assert.assertEquals(wwelcomeMsg,"Welcome to your account. Here you can manage all of your personal information and orders.");
 		
 		//Verify 'My personal information' text displayed
@@ -43,6 +53,11 @@ public class LoginTests extends TestBase{
 
 	}
 
-
+	public void logintc2() {
+		
+	}
+	public void logintc3() {
+		
+	}
 	
 }
